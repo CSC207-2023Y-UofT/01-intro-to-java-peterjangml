15bdde1cd22e255b571f1f702da2e0c31375423d
peterjangml <hspeterj@gmail.com>
2023-06-08 02:53:14 -0400

    second submission (added missing ;)

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -98,8 +98,8 @@ public class Basics {
          * Current count: 1
          * Current count: 0
          */
-        for (int i = 10; i >= 0; i--){
-            System.out.println("Current count: " + i)
+        for (int i = 10; i >= 0; i--) {
+            System.out.println("Current count: " + i);
         }
 
     }
